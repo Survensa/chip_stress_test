a0d7fa907bbb7306d3b96a65426f84f9e673dc3b
Suraj Seenivasan <95559513+Survensa@users.noreply.github.com>
2023-07-07 17:31:17 +0530

    Update Threadreset.py

--- Threadreset.py
+++ Threadreset.py
@@ -3,20 +3,17 @@ import time
 import logging
 import os
 import json
+import subprocess
 
-def thread_reset():
+def thread_reset(data):
 
-   data ={}
-
-   with open('config.json') as f:
-       data = json.load(f)
 
    local_file_path = os.path.abspath(os.getcwd()) + '/thread.py'
 
    remote_file_path = data['path']
 
 
-   ssh = Connection(host=data['host'], user=data['username'] , connect_kwargs={"password": data['password']})
+   ssh = Connection(host=data['host'], user=data['user'] , connect_kwargs={"password": data['password']})
 
    ssh.put(local_file_path, remote_file_path)
 
@@ -27,10 +24,17 @@ def thread_reset():
 
    ssh.run(f'rm '+remote_file_path+'/thread.py')
 
+   ssh.close()
+
+   srpdisable = 'docker exec -ti otbr ot-ctl srp server disable'
+   subprocess.run(srpdisable , shell=True)
+   time.sleep(1)
+   srpenable = 'docker exec -ti otbr ot-ctl srp server enable'
+   subprocess.run(srpenable , shell=True)
+
    logging.info("Completed the reset")
 
 
-   ssh.close()
 
 
 

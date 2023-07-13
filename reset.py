20b4671b031a4047c74a5156c9cda6ef719911af
Saravana Perumal <104007654+Saravana-kr22@users.noreply.github.com>
2023-07-13 19:17:47 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -3,8 +3,10 @@ import logging
 from fabric import Connection
 from invoke import UnexpectedExit
 from abc import ABC, abstractmethod
-from config import config_data
+from config import rpi_config, thread_config
 import threading
+import subprocess
+import time
 
 class Reset (ABC):
 
@@ -27,9 +29,9 @@ class Rpi(Reset):
 
     def reboot(self):
         
-        data = config_data()
+        data = rpi_config()
         
-        ssh = Connection(host=data['host'], user=data['username'] , connect_kwargs={"password": data['password']})
+        ssh = Connection(host=data.host, user=data.username , connect_kwargs={"password": data.password})
         
         reboot_command = f"sudo reboot"
 
@@ -43,18 +45,18 @@ class Rpi(Reset):
     def factory_reset(self, i):
         
              
-        data = config_data()
+        data = rpi_config()
         
-        ssh = Connection(host=data['host'], user=data['username'] , connect_kwargs={"password": data['password']})
+        ssh = Connection(host=data.host, user=data.username , connect_kwargs={"password": data.password})
         
-        # Execute 'ps aux | grep process_name' command to find the PID
-        command = f"ps aux | grep ./chip-all-clusters-app"
+        # Executing the  'ps aux | grep process_name' command to find the PID value to kill
+        command = f"ps aux | grep {data.command}"
         pid_val = ssh.run(command)
 
         pid_output = pid_val.stdout
         pid_lines = pid_output.split('\n')
         for line in pid_lines :
-            if './chip-all-clusters-app' in line:
+            if data.command in line:
                 pid = line.split()[1]
                 kill_command = f"kill -9 {pid}"
                 ssh.run(kill_command)
@@ -70,12 +72,12 @@ class Rpi(Reset):
 
     def advertise(self):
 
-        data = config_data()
+        data = rpi_config()
 
-        ssh = Connection(host=data['host'], user=data['username'] , connect_kwargs={"password": data['password']})
-        path  = data['path']
+        ssh = Connection(host=data.host, user=data.username , connect_kwargs={"password": data.password})
+        path  = data.path
         try:
-                ssh.run('cd ' + path +' && ./chip-all-clusters-app ' , hide =True, pty=False)
+                ssh.run('cd ' + path +' && '+ data.command , hide =True, pty=False)
         except UnexpectedExit as e:
                 if e.result.exited == -1:
                         None
@@ -91,10 +93,25 @@ class Rpi(Reset):
 class Nordic(Reset):
      
     def reboot(self):
-        return super().reboot()
+        #As of now the nRF52840-DK is not able to reboot
+        return (self.factory_reset())
      
     def factory_reset(self):
-        return super().factory_reset()
+
+        data = thread_config()
+
+        if data == False:
+            cmd = f'echo -e "matter device factoryreset" > {data.port}'
+            subprocess.run(cmd , shell=True)
+
+        else:
+            ssh =  Connection(host=data.host, user=data.username , connect_kwargs={"password": data.password})     
+            ssh.run(f'echo -e "matter device factoryreset" > {data.port}')
+
+        time.sleep(2)
+
+        return True
      
     def advertise(self):
-        return super().advertise()
+        #Since the advertisement is done during factory_reset it can be skipped
+        return True
